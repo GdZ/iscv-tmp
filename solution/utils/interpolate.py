d54aaf168a22175487fe6bb1d62869ede43df81e
ysLiu <alanniliu0717@gmail.com>
2020-07-09 18:39:51 +0200

    interpolate function for matrix input

--- solution/utils/interpolate.py
+++ solution/utils/interpolate.py
@@ -4,16 +4,17 @@ import numpy as np
 def bilinear_interpolate(im, x, y):
     x = np.asarray(x)
     y = np.asarray(y)
+    im = np.asarray(im)
 
-    x0 = np.floor(x).astype(int)
+    x0 = np.ceil(x-1).astype(int)
+    x0 = np.clip(x0, 0, im.shape[1]-1)
     x1 = x0 + 1
-    y0 = np.floor(y).astype(int)
-    y1 = y0 + 1
+    x1 = np.clip(x1, 0, im.shape[1]-1)
 
-    x0 = np.clip(x0, 0, im.shape[1] - 1)
-    x1 = np.clip(x1, 0, im.shape[1] - 1)
-    y0 = np.clip(y0, 0, im.shape[0] - 1)
-    y1 = np.clip(y1, 0, im.shape[0] - 1)
+    y0 = np.ceil(y-1).astype(int)
+    y0 = np.clip(y0, 0, im.shape[0]-1)
+    y1 = y0 + 1
+    y1 = np.clip(y1, 0, im.shape[0]-1)
 
     Ia = im[y0, x0]
     Ib = im[y1, x0]
@@ -25,11 +26,32 @@ def bilinear_interpolate(im, x, y):
     wc = (x - x0) * (y1 - y)
     wd = (x - x0) * (y - y0)
 
-    # tmp = np.zeros_like(im)
-    tmp = 0
-    if x > im.shape[1]-1 or y > im.shape[0]-1 or x < 0 or y < 0:
-        tmp = np.nan
-    else:
-        tmp = wa * Ia + wb * Ib + wc * Ic + wd * Id
+    # print(wa)
+
+    tmp = (wa * Ia + wb * Ib + wc * Ic + wd * Id).astype(float)
+    tmp[x < 0] = np.nan
+    tmp[y < 0] = np.nan
+    tmp[x > im.shape[1] - 1] = np.nan
+    tmp[y > im.shape[0] - 1] = np.nan
+    # print(tmp)
+    # tmp[x > im.shape[1] - 1] = np.nan
+    # tmp[y > im.shape[0]-1] = np.nan
+    # tmp[x < 0] = np.nan
+    # tmp[y < 0] = np.nan
+    #    if x > im.shape[1]-1 or y > im.shape[0]-1 or x < 0 or y < 0:
+    #        tmp = np.nan
+    #   else:
+    #       tmp = wa * Ia + wb * Ib + wc * Ic + wd * Id
 
     return tmp
+
+n = [[54.5, 17.041667, 31.993],
+     [54.5, 17.083333, 31.911],
+     [54.458333, 17.041667, 31.945],
+     [54.458333, 17.083333, 31.866],
+     ]
+x = np.repeat([0,1,2,-5],3).reshape(4,3)
+y = np.tile([0,1,2],4).reshape(4,3)
+
+
+print(bilinear_interpolate(n,y,x))
